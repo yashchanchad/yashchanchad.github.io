acf09e6f704ef950465b05595c71704b2972d743
yashchanchad <44048250+yashchanchad@users.noreply.github.com>
2020-07-26 17:05:11 -0700

    Added built of website

---
+++ precache-manifest.f6dda5d6d1cc8d2c0cfef0a71590472c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed2013de0775b80bd74242f74879252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e587649cfc54c19a9465",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "750a0188d506d71212df",
+    "url": "/static/css/main.27eb7adb.chunk.css"
+  },
+  {
+    "revision": "e587649cfc54c19a9465",
+    "url": "/static/js/2.acbc2e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acbc2e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750a0188d506d71212df",
+    "url": "/static/js/main.81694a8a.chunk.js"
+  },
+  {
+    "revision": "4acfa0337c4869929a9d",
+    "url": "/static/js/runtime-main.1552d3ae.js"
+  },
+  {
+    "revision": "b6770cc0dc4c4f686d8209567c12ebf0",
+    "url": "/static/media/Resume.b6770cc0.pdf"
+  },
+  {
+    "revision": "0694d0ca2f43b09506b12b2cda7e1a75",
+    "url": "/static/media/Yash_bitmoji.0694d0ca.png"
+  }
+]);
